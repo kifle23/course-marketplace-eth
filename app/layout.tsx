c7f0ca3710ca4df13af6112bb9be36f70a1372fd
kifle23 <kiflekifle23@gmail.com>
2024-07-05 14:30:32 +0300

    Update import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
-import Nav from "@components/nav.component";
-import Footer from "@components/footer.component";
+import { Footer, Nav } from "@components/common";
 
 const inter = Inter({ subsets: ["latin"] });
 
