4cfbd1f96c6ba7cd53c24847c125cb11317ac581
kifle23 <kiflekifle23@gmail.com>
2024-07-05 11:33:40 +0300

    Refactor and create footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
 import Nav from "@components/nav.component";
+import Footer from "@components/footer.component";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,17 +25,7 @@ export default function RootLayout({
             {children}
           </div>
         </main>
-        <footer className="bg-gray-900 pt-1">
-          <div className="container mx-auto px-6">
-            <div className="mt-5 flex flex-col items-center">
-              <div className="py-6">
-                <p className="mb-6 text-white text-sm text-primary-2 font-bold">
-                  © {new Date().getFullYear()}
-                </p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
