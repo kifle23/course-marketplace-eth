b09adf579252744ae39ccdac44b6a1c43e154f2c
kifle23 <kiflekifle23@gmail.com>
2024-07-05 14:30:16 +0300

    Create common exporter

---
+++ components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as Footer } from "./footer.component";
+export { default as Nav } from "./nav.component";
+export { default as Breadcrumbs } from "./breadcrumbs.component";
+export { default as Hero } from "./hero.component";
+
