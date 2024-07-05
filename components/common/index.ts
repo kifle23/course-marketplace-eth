bfa2528673c3ddcdf6fa43e353d0c22252490ea1
kifle23 <kiflekifle23@gmail.com>
2024-07-05 15:25:27 +0300

    Update common exporter

--- components/common/index.ts
+++ components/common/index.ts
@@ -2,4 +2,5 @@ export { default as Footer } from "./footer.component";
 export { default as Nav } from "./nav.component";
 export { default as Breadcrumbs } from "./breadcrumbs.component";
 export { default as Hero } from "./hero.component";
+export { default as Modal } from "./modal.component";
 
