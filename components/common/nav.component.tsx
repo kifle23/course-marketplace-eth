9149fd53899b9886c19c873fd5c92dfc6be4fb3d
kifle23 <kiflekifle23@gmail.com>
2024-07-06 10:53:42 +0300

    Update nav links

--- components/common/nav.component.tsx
+++ components/common/nav.component.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 
 export default function Nav() {
   return (
@@ -7,38 +8,38 @@ export default function Nav() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
             <div>
-              <a
-                href="#"
+              <Link
+                href="/"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
-                Product
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Features
-              </a>
-              <a
-                href="#"
+                Home
+              </Link>
+              <Link
+                href="/marketplace"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
                 Marketplace
-              </a>
-            </div>
-            <div>
-              <a
-                href="#"
+              </Link>
+              <Link
+                href="/blogs"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
-                Company
-              </a>
-              <a
-                href="#"
+                Blogs
+              </Link>
+            </div>
+            <div>
+              <Link
+                href="/wishlist"
+                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
+              >
+                Wishlist
+              </Link>
+              <Link
+                href="/login"
                 className="font-medium mr-8 text-indigo-600 hover:text-indigo-500"
               >
                 Log in
-              </a>
+              </Link>
             </div>
           </div>
         </nav>
