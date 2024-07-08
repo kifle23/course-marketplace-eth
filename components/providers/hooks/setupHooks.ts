96e145c65443b8bfd563b829041e391455cd70c7
kifle23 <kiflekifle23@gmail.com>
2024-07-08 18:23:53 +0300

    Get accounts

--- components/providers/hooks/setupHooks.ts
+++ components/providers/hooks/setupHooks.ts
@@ -1,5 +1,5 @@
 import Web3 from "web3";
-import { accountHandler as createUseAccount } from "./useAccount";
+import { AccountHandler as createUseAccount } from "./useAccount";
 interface setupHooksProps {
   web3: Web3 | null;
 }
--- components/providers/hooks/useAccount.ts
+++ components/providers/hooks/useAccount.ts
@@ -1,17 +1,28 @@
+import { use, useEffect, useState } from "react";
 import Web3 from "web3";
 
 interface UseAccountProps {
-    web3: Web3 | null;
+  web3: Web3 | null;
 }
 
 interface Account {
   account: string;
 }
 
-export const accountHandler = ({ web3 }: UseAccountProps) => {
-    const getAccount = (): Account => {
-        return { account: web3 ? "Test Account" : "null" };
-    };
+export const AccountHandler = ({ web3 }: UseAccountProps) => {
+  const [account, setAccount] = useState<string | null>(null);
 
-    return getAccount;
+  useEffect(() => {
+    if (web3) {
+      web3.eth.getAccounts().then((accounts) => {
+        setAccount(accounts[0]);
+      });
+    }
+  }, [web3]);
+
+  const getAccount = (): Account => {
+    return { account: account || "" };
+  };
+
+  return getAccount;
 };
