ca720d99a36678ab4a0258592d39d2819a74507d
kifle23 <kiflekifle23@gmail.com>
2024-07-08 20:12:47 +0300

    Display current account

--- components/ui/common/nav.component.tsx
+++ components/ui/common/nav.component.tsx
@@ -12,7 +12,6 @@ export default function Nav() {
 
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
@@ -137,6 +136,13 @@ export default function Nav() {
           )}
         </nav>
       </div>
+      {account && (
+        <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            {account}
+          </div>
+        </div>
+      )}
     </section>
   );
 }
