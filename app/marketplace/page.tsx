ae88947a5a65e96a4d3a60f37a63af55502be6b0
kifle23 <kiflekifle23@gmail.com>
2024-07-09 19:33:16 +0300

    Add course card component

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,16 +1,16 @@
 import { CourseList } from "@components/ui/course";
 import { WalletBar } from "@components/ui/web3";
 import { getAllCourses } from "@content/courses/fetcher";
-import { useNetwork } from "@components/hooks/web3/useNetwork";
 
 export default function Marketplace() {
   const { data: courses } = getAllCourses();
+
   return (
     <>
       <div className="py-4">
         <WalletBar />
       </div>
-      <CourseList courses={courses} />
+      <CourseList courses={courses} useCustomCard={true} />
     </>
   );
 }
---
+++ components/ui/course/card.component.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Course } from "@content/courses/types";
+import Link from "next/link";
+
+interface CardProps {
+  course: Course;
+}
+
+export default function Card({ course }: CardProps) {
+  return (
+    <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="flex">
+        <div className="relative w-1/3 h-56">
+          <Image
+            className="object-cover"
+            src={course.coverImage}
+            alt={course.title}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority
+          />
+        </div>
+        <div className="w-2/3 p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            {course.type}
+          </div>
+          <Link
+            href={`/course/${course.slug}`}
+            className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+          >
+            {course.title}
+          </Link>
+          <p className="mt-2 text-gray-500">{course.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
