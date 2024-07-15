39c28c4534665d74847f88bade0ff576b4da6936
kifle23 <kiflekifle23@gmail.com>
2024-07-15 10:53:05 +0300

    Update marketplace course page

--- app/marketplace/courses/owned/page.tsx
+++ app/marketplace/courses/owned/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { MarketHeader } from "@components/ui/marketplace";
 
@@ -8,7 +9,9 @@ export default function OwnedCourses() {
         <MarketHeader />
       </div>
       <section className="grid grid-cols-1">
-        <OwnedCourseCard />
+        <OwnedCourseCard>
+          <Button>Watch the course</Button>
+        </OwnedCourseCard>
       </section>
     </>
   );
