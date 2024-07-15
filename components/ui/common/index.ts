933808d3755c139d58614904572d67b8c50af5de
kifle23 <kiflekifle23@gmail.com>
2024-07-15 11:42:11 +0300

    Add message component

--- components/ui/common/index.ts
+++ components/ui/common/index.ts
@@ -6,4 +6,5 @@ export { default as Modal } from "./modal.component";
 export { default as NotFound } from "../../../app/not-found";
 export { default as Button } from "./button.component";
 export { default as ActiveLink } from "./link.component";
+export { default as Message } from "./message.component";
 
