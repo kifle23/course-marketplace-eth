64a456c8c732a21d43ee8bd5e13b06458868d02a
kifle23 <kiflekifle23@gmail.com>
2024-07-21 13:46:00 +0300

    chore: Refactor useOwnedCourses hook to accept courses and account parameters

--- components/providers/web3/hooks/useOwnedCourses.ts
+++ components/providers/web3/hooks/useOwnedCourses.ts
@@ -1,15 +1,29 @@
+import { Course } from "@content/courses/types";
+import useSWR from "swr";
 import Web3 from "web3";
 
 interface UseOwnedCoursesProps {
-    web3: Web3 | null;
-    contract: any;
-  }
-  
-export const OwnedCoursesHandler = ({
-    web3,
-    contract,
-  }: UseOwnedCoursesProps)=>() => {
-    
-    return "useOwnedCourses is working!";
-    };
+  web3: Web3 | null;
+  contract: any;
+  courses: Course[];
+  account: string;
+}
 
+const fetchOwnedCourses = async (web3: Web3, contract: any, account: string, courses: Course[]) => {
+  const ownedCourses = [];
+  for (const course of courses) {
+    ownedCourses.push(course.id);
+  }
+  return ownedCourses;
+};
+
+export const OwnedCoursesHandler = ({ web3, contract, courses, account }: UseOwnedCoursesProps) => {
+  const shouldFetch = web3 && contract && account;
+
+  const swrRes = useSWR(
+    shouldFetch ? `web3/ownedCourses/${account}` : null,
+    () => fetchOwnedCourses(web3!, contract, account, courses)
+  );
+
+  return swrRes;
+};
