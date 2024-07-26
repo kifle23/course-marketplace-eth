30e5287b47b9e5a012bebaed2fa0baf6edae5e7d
kifle23 <kiflekifle23@gmail.com>
2024-07-26 17:04:10 +0300

    Refactor iWalletInfo interface to remove unnecessary nesting

--- interfaces/iWalletInfo.ts
+++ interfaces/iWalletInfo.ts
@@ -1,18 +1,15 @@
 export interface Account {
-  account: {
-    data: string;
-    isAdmin: boolean;
-    mutate: () => void;
-    isInitialized: boolean;
-    isEmpty?: boolean;
-  };
+  data: string;
+  isAdmin: boolean;
+  mutate: () => void;
+  isInitialized?: boolean;
+  isEmpty?: boolean;
 }
 
 export interface Network {
-  network: {
-    data: string;
-    target: string;
-    isSupported: boolean;
-    isInitialized: boolean;
-  };
+  data: string;
+  target: string;
+  isSupported: boolean;
+  isInitialized?: boolean;
+  isEmpty?: boolean;
 }
