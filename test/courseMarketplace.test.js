d839fc0ef218035741e2274a4f2eec9812cb0578
kifle23 <kiflekifle23@gmail.com>
2024-07-28 22:08:02 +0300

    feat: Add test course by index functionality

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -1,23 +1,39 @@
 const CourseMarketplace = artifacts.require('CourseMarketplace');
 
 contract('CourseMarketplace', accounts => {
+
+    const courseId = "0x00000000000000000000000000003130";
+    const proof = "0x0000000000000000000000000000313000000000000000000000000000003130";
+    const value = "900000000";
+
     let _contract = null;
-    let _owner = null;
-    let _buyer = null;
+    let owner = null;
+    let buyer = null;
 
     before(async () => {
         _contract = await CourseMarketplace.deployed();
-        _owner = accounts[0];
-        _buyer = accounts[1];
-
-        console.log('Contract deployed at:', _contract.address);
-        console.log('Owner:', _owner);
-        console.log('Buyer:', _buyer);
+        owner = accounts[0];
+        buyer = accounts[1];
     });
 
-    describe('CourseMarketplace', () => {
-        it('should deploy', async () => {
-            assert(_contract.address);
+    describe("Purchase the new course", () => {
+
+        before(async () => {
+            await _contract.purchaseCourse(courseId, proof, {
+                from: buyer,
+                value
+            })
+        });
+
+        it("can get the purchased course hash by index", async () => {
+            const index = 0;
+            courseHash = await _contract.getCourseHashAtIndex(index);
+            const expectedHash = web3.utils.soliditySha3(
+                { type: "bytes16", value: courseId },
+                { type: "address", value: buyer },
+            );
+
+            assert.equal(courseHash, expectedHash, "Course hash is not maching the hash of purchased course!");
         });
     });
 });
