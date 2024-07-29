f6434cd3538a0a3ae4d5e84020e9e3b124e389a7
kifle23 <kiflekifle23@gmail.com>
2024-07-29 16:44:43 +0300

    feat: Add feature to activate course

--- components/ui/course/managed.component.tsx
+++ components/ui/course/managed.component.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { useAdmin, useManagedCourses } from "@components/hooks/web3";
 import { CourseFilter, ManagedCourseCard } from "@components/ui/course";
-import { Message } from "@components/ui/common";
+import { Button, Message } from "@components/ui/common";
 import { useState } from "react";
-import web3 from "web3";
+import { useWeb3 } from "@components/providers";
 import VerificationInput from "@components/ui/course/verification-input.component";
 import { Course } from "@content/courses/types";
 
 export default function ManageWrapper() {
+  const { web3, contract } = useWeb3();
   const account = useAdmin({ redirectTo: "/marketplace" });
   const { managedCourses } = useManagedCourses(account);
   const [proofedOwnership, setProofedOwnership] = useState<{
@@ -19,8 +20,8 @@ export default function ManageWrapper() {
     arg1: { hash: string | undefined; proof: string | undefined }
   ) {
     const { hash, proof } = arg1;
-    const emailHash = web3.utils.sha3(email);
-    const proofToVerify = web3.utils.soliditySha3(
+    const emailHash = web3?.utils.sha3(email);
+    const proofToVerify = web3?.utils.soliditySha3(
       { t: "bytes32", v: emailHash },
       { t: "bytes32", v: hash }
     );
@@ -35,6 +36,24 @@ export default function ManageWrapper() {
           [hash?.toString() ?? ""]: false,
         });
   }
+
+  const activateCourse = async ({
+    courseHash,
+  }: {
+    courseHash: string | undefined;
+  }) => {
+    if (!courseHash) {
+      return;
+    }
+    try {
+      await contract.methods.activateCourse(courseHash).send({
+        from: account.data,
+        gas: 200000,
+      });
+    } catch (e) {
+      console.error((e as Error).message);
+    }
+  };
 
   if (!account.isAdmin) return null;
 
@@ -64,6 +83,17 @@ export default function ManageWrapper() {
               {proofedOwnership[course.hash?.toString() ?? ""] === false && (
                 <div className="mt-2">
                   <Message type="danger">Ownership not verified</Message>
+                </div>
+              )}
+              {course.state === "purchased" && (
+                <div className="mt-2">
+                  <Button
+                    onClick={() => activateCourse({ courseHash: course.hash })}
+                    variant="green"
+                  >
+                    Activate
+                  </Button>
+                  <Button variant="red">Deactivate</Button>
                 </div>
               )}
             </div>
