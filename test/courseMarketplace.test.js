40e623e5094dabdd3eb1ea327b29d060bf4ab0f2
kifle23 <kiflekifle23@gmail.com>
2024-07-29 17:53:51 +0300

    feat: Check buyer balance on repurchase course test case

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -4,6 +4,17 @@ const { catchRevert } = require('./utils/exceptions');
 // Mocha - testing framework
 // Chai - assertion JS library
 
+const getBalance = async address => web3.eth.getBalance(address);
+const toBN = value => web3.utils.toBN(value);
+
+const getGas = async result => {
+    const tx = await web3.eth.getTransaction(result.tx);
+    const gasUsed = toBN(result.receipt.gasUsed);
+    const gasPrice = toBN(tx.gasPrice);
+    const gas = gasUsed.mul(gasPrice);
+    return gas;
+};
+
 contract('CourseMarketplace', accounts => {
 
     const courseId = "0x00000000000000000000000000003130";
@@ -161,12 +172,32 @@ contract('CourseMarketplace', accounts => {
         });
 
         it("should be able repurchase with the original buyer", async () => {
-            await _contract.repurchaseCourse(courseHash2, { from: buyer, value });
+            const beforeTxBuyerBalance = await getBalance(buyer);
+            const beforeTxContractBalance = await getBalance(_contract.address);
+
+            const result = await _contract.repurchaseCourse(courseHash2, { from: buyer, value });
+
+            const afterTxBuyerBalance = await getBalance(buyer);
+            const afterTxContractBalance = await getBalance(_contract.address);
+
             const course = await _contract.getCourseByHash(courseHash2);
             const exptectedState = 0;
+            const gas = await getGas(result);
 
             assert.equal(course.state, exptectedState, "The course is not in purchased state");
             assert.equal(course.price, value, `The course price is not equal to ${value}`);
+
+            assert.equal(
+                toBN(beforeTxBuyerBalance).sub(toBN(value)).sub(gas).toString(),
+                afterTxBuyerBalance,
+                "Client balance is not correct!"
+            );
+
+            assert.equal(
+                toBN(beforeTxContractBalance).add(toBN(value)).toString(),
+                afterTxContractBalance,
+                "Contract balance is not correct!"
+            );
         });
 
         it("should NOT be able to repurchase purchased course", async () => {
